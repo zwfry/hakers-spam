cc87bc5118f6d8612104db03c28b7e9a98f74566
ZWFRY ISMAIL <55112227+zwfry@users.noreply.github.com>
2019-10-09 22:10:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client("NjI5MzA4NjQ2MjkxMTQ0NzE0.XZ4_5A.yrXvMDxpGC3lBhrBnpZOkR6pwSg");
 console.log("Scrpit By Dream");
 
 
@@ -10,4 +10,4 @@ channel.send(`EL MAJORE 9ALIKOM HAD RISALA {Fuck you sons of prostitutes}`);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login("NjI5MzA4NjQ2MjkxMTQ0NzE0.XZ4_5A.yrXvMDxpGC3lBhrBnpZOkR6pwSg");
